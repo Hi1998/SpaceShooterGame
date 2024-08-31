8c4d058f0492ed6637f5d76763c70fdbdc4f6ba4
Himanshu Lal <himanshulal191@gmail.com>
2024-08-31 23:03:00 +0530

    Added particle system when bullet hit enemy then one particle emit it just the initial process need to work more on this

--- Assets/Scripts/BulletCollisionDetector.cs
+++ Assets/Scripts/BulletCollisionDetector.cs
@@ -4,10 +4,31 @@ using UnityEngine;
 
 public class BulletCollisionDetector : MonoBehaviour
 {
+    /// <summary>
+    /// Enemy hit animation when bullet hit the enemies
+    /// </summary>
+    [SerializeField]
+    private GameObject collisionParticleprefab;
+
+    void Update()
+    {
+    }
     void OnCollisionEnter(Collision collision)
     {
         if(collision.gameObject.tag == "Enemy")
         {
+            // Get the contact point of the collision
+            Vector2 collisionPoint = collision.contacts[0].point;
+
+            // Instantiate the particle system at the collision point
+            GameObject particleSystemInstance = Instantiate(collisionParticleprefab, collisionPoint, Quaternion.identity);
+            // Ensure the particle system plays immediately
+            ParticleSystem particleSystem = particleSystemInstance.GetComponent<ParticleSystem>();
+            if (particleSystem != null)
+            {
+                particleSystem.Play(); // Explicitly start the particle system
+            }
+            Destroy(particleSystemInstance, particleSystem.main.duration);
             Destroy(collision.gameObject);
         }
     }
