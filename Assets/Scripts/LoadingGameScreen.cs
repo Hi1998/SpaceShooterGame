8312c68c8fd69d959414cfb465f23de3878cb440
Himanshu Lal <himanshulal191@gmail.com>
2024-09-01 20:05:49 +0530

    Added comment to the LoadingGameScreen.cs

--- Assets/Scripts/LoadingGameScreen.cs
+++ Assets/Scripts/LoadingGameScreen.cs
@@ -5,6 +5,10 @@ using UnityEngine.SceneManagement;
 
 public class LoadingGameScreen : MonoBehaviour
 {
+
+    /// <summary>
+    /// Here we load the Game Screen on clicking the play button
+    /// </summary>
     public void StartTheGame()
     {
         SceneManager.LoadScene("SampleScene");
